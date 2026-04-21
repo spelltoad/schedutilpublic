fa3d071fc954c4c57ac152e75255dcaeee4c3145
spelltoad <timst2003@yandex.ru>
2026-04-21 14:50:00 +0300

    add .env secret

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(join(dirname(__file__), '.env'))
+
 class Config:
-    SECRET_KEY = 'nevergonnagive'
+    SECRET_KEY = os.environ.get('SECRET_KEY') 
+    if not SECRET_KEY:
+        raise ValueError("No SECRET_KEY set")
